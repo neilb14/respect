4ed246f632886c966fa4114c13d4b7bf4a85e520
Neil Bourgeois <neil.bourgeois@gmail.com>
2011-12-15 10:27:28 -0700

    added format to features list so we can dogfood

---
+++ features/feature_list/list_all_features.js
@@ -0,0 +1,8 @@
+test = {
+	description: "The list of features",
+	url: "/respect/features.js",
+	method: "GET",
+	verify: function(result){
+		console.log('Verifying the list of features...');
+	}
+};
