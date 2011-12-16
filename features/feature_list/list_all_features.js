68c51dd95ef6a2be11c31387aeee270033c45150
Neil Bourgeois <neil.bourgeois@gmail.com>
2011-12-16 16:16:02 -0700

    moved test request code into main handler

--- features/feature_list/list_all_features.js
+++ features/feature_list/list_all_features.js
@@ -1,24 +1,9 @@
 test = {
 	description: "The list of features",
 	url: "/respect/features.js",
-	verify: function(){
-		var options = {
-			host: 'localhost',
-			port: 8818,
-			path: "/respect/features.js",
-			method: 'GET'
-		};
-		var req = http.request(options,function(res) {
-								console.log("STATUS: " + res.statusCode);	
-								console.log("HEADERS: " + JSON.stringify(res.headers));	
-								res.setEncoding('utf8');
-								res.on('data', function(chunk) {
-									console.log("BODY: " + chunk);
-								});
-							});		
-		req.on('error', function(e) {
-			console.log('Problem with request: ' + e.message);
-		});
-		req.end();
+	method: "GET",
+	verify: function(res, data){
+		console.log("Verifying: " + data);
+		return "Pending";
 	}
 };
