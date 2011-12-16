08054c2431ed8ba608a0c05d7752acbdefb3fa68
Neil Bourgeois <neil.bourgeois@gmail.com>
2011-12-16 15:48:29 -0700

    make http request for test

--- features/feature_list/list_all_features.js
+++ features/feature_list/list_all_features.js
@@ -1,9 +1,24 @@
 test = {
 	description: "The list of features",
 	url: "/respect/features.js",
-	method: "GET",
-	verify: function(result){
-		console.log('Verifying the list of features...');
-		return 'Success!';
+	verify: function(){
+		var options = {
+			host: 'localhost',
+			port: 8818,
+			path: "/respect/features.js",
+			method: 'GET'
+		};
+		var req = http.request(options,function(res) {
+								console.log("STATUS: " + res.statusCode);	
+								console.log("HEADERS: " + JSON.stringify(res.headers));	
+								res.setEncoding('utf8');
+								res.on('data', function(chunk) {
+									console.log("BODY: " + chunk);
+								});
+							});		
+		req.on('error', function(e) {
+			console.log('Problem with request: ' + e.message);
+		});
+		req.end();
 	}
 };
