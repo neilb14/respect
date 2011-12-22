0b062e15057929418dd733fc268c9097f6fe799b
Neil Bourgeois <nbourgeois@tycothermal.com>
2011-12-22 16:53:50 -0700

    pushed jasmine async waits into rest helper

--- features/test.spec.js
+++ features/test.spec.js
@@ -3,8 +3,6 @@ describe('anything', function() {
         get("/respect/features.js", function(res, body){
             expect(res.statusCode).toEqual(200);
     		expect(body.title).toEqual("Dogfooding Respect");
-    	}, function(e){
-            console.error(e.message);
-        });
+    	});
 	});
 });
