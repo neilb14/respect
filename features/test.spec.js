06f2381e372dfb7591775e780c58c624865f3a90
Neil Bourgeois <nbourgeois@tycothermal.com>
2011-12-22 16:43:20 -0700

    fixed rest helpers so they run sync

--- features/test.spec.js
+++ features/test.spec.js
@@ -1,8 +1,10 @@
 describe('anything', function() {
 	it('should get all features', function(){
-		get("/respect/features.js", function(res, body){
-            expect(res.status).toEqual(200);
-			expect(body.title).toEqual("Dogfooding Respect");
-		});
+        get("/respect/features.js", function(res, body){
+            expect(res.statusCode).toEqual(200);
+    		expect(body.title).toEqual("Dogfooding Respect");
+    	}, function(e){
+            console.error(e.message);
+        });
 	});
 });
