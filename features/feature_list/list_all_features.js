e5efd8c93baac8443000318dd640129ef4113047
Neil Bourgeois <neil.bourgeois@gmail.com>
2011-12-22 17:36:41 -0700

    changed from custom feature files to Jasmine specs

--- features/feature_list/list_all_features.js
+++
@@ -1,9 +0,0 @@
-test = {
-	description: "The list of features",
-	url: "/respect/features.js",
-	method: "GET",
-	verify: function(res, data){
-		console.log("Verifying: " + data);
-		return "Pending";
-	}
-};
