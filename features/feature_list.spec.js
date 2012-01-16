abf71edc4390c91f2fea2dbc031fa8596841a502
Neil Bourgeois <neil.bourgeois@gmail.com>
2012-01-16 04:46:00 -0700

    show all specs

--- features/feature_list.spec.js
+++ features/feature_list.spec.js
@@ -3,7 +3,14 @@ describe('feature list', function() {
         get("/respect/features.js", function(res, body){
           expect(res.statusCode).toEqual(200);
 					expect(body.title).toEqual("Dogfooding Respect");
-					expect(body.features).toContain("feature_list");
+					expect(body.features.length).toEqual(1);
     	});
+	});
+	
+	it('should allow a user to view a specific feature', function() {
+		get("/respect/features.js", function(res, body) {
+			expect(body.features[0].name).toEqual("feature_list");
+			expect(body.features[0].showUrl).toEqual("/respect/features/feature_list");
+		});
 	});
 });
