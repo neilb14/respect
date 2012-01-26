9f2f4154e8852d360c391cea7527b88298968b3a
Neil Bourgeois <neil.bourgeois@gmail.com>
2012-01-26 01:53:18 -0700

    added some meaningful tests to show feature

--- features/show_feature.spec.js
+++ features/show_feature.spec.js
@@ -3,6 +3,9 @@ describe('show feature', function() {
         get("/respect/show_feature/show_feature.js", function(res, body){
           expect(res.statusCode).toEqual(200);
 					expect(body.title).toEqual("Dogfooding Respect");
+					expect(body.feature).toEqual("show_feature");
+					expect(body.runUrl).toEqual("/respect/feature/show_feature");
+					expect(body.specs.length).toEqual(1);
    		});
    	});
 });
