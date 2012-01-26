aa886a82a709bff8ab1ff5e547a3f6228f768b98
Neil Bourgeois <neil.bourgeois@gmail.com>
2012-01-26 01:59:47 -0700

    adding more tests to show feature

--- features/show_feature.spec.js
+++ features/show_feature.spec.js
@@ -1,11 +1,21 @@
 describe('show feature', function() {
-	it('should list all specs', function(){
-        get("/respect/show_feature/show_feature.js", function(res, body){
-          expect(res.statusCode).toEqual(200);
-					expect(body.title).toEqual("Dogfooding Respect");
-					expect(body.feature).toEqual("show_feature");
-					expect(body.runUrl).toEqual("/respect/feature/show_feature");
-					expect(body.specs.length).toEqual(1);
-   		});
-   	});
+	it('should return an HTTP status of 200', function(){
+		get("/respect/show_feature/show_feature.js", function(res,body){
+			expect(res.statusCode).toEqual(200);
+		});
+	});
+
+	it('should list all specs defined for the feature', function(){
+    get("/respect/show_feature/show_feature.js", function(res, body){
+			expect(body.title).toEqual("Dogfooding Respect");
+			expect(body.feature).toEqual("show_feature");
+			expect(body.specs.length).toEqual(3);
+ 		});
+  });
+
+	it('should provide a url that an HTTP POST may be sent to to run the specs', function() {
+		get("/respect/show_feature/show_feature.js", function(res,body) {
+			expect(body.runUrl).toEqual("/respect/feature/show_feature");
+		});
+	});
 });
