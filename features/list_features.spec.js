13a2ba331b378076750c480b2ae535f087fb6645
Neil Bourgeois <neil.bourgeois@gmail.com>
2012-01-26 01:39:03 -0700

    introduced some naming convention (list/show)

---
+++ features/list_features.spec.js
@@ -0,0 +1,16 @@
+describe('list features', function() {
+	it('should list all defined features', function(){
+        get("/respect/list_features.js", function(res, body){
+          expect(res.statusCode).toEqual(200);
+					expect(body.title).toEqual("Dogfooding Respect");
+					expect(body.features.length).toEqual(2); // change this to be greater than :-)
+    	});
+	});
+	
+	it('should allow a user to view a specific feature', function() {
+		get("/respect/list_features.js", function(res, body) {
+			expect(body.features[0].name).toEqual("list_features");
+			expect(body.features[0].showUrl).toEqual("/respect/show_feature/list_feature");
+		});
+	});
+});
---
+++ features/show_feature.spec.js
@@ -0,0 +1,8 @@
+describe('show feature', function() {
+	it('should list all specs', function(){
+        get("/respect/show_feature/show_feature.js", function(res, body){
+          expect(res.statusCode).toEqual(200);
+					expect(body.title).toEqual("Dogfooding Respect");
+    	});
+	});
+});
